4e746cef85dd69a1c9b920e2d118ab1a02a65d85
a2nis <a2nistamayo@gmail.com>
2022-12-21 13:24:44 -0300

    Improvements to the method get data from the file

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
 import barcode
 
-def generate_ean(valor):
-  if len(valor) == 13: 
+def generate_ean(value):
+  if len(value) == 13: 
     hr = barcode.get_barcode_class('ean13')
-    Hr = hr(valor)  #8430469156277
-    qr = Hr.save(valor)
-  if len(valor) == 8:
+    Hr = hr(value)  #8430469156277
+    qr = Hr.save(value)
+  if len(value) == 8:
     ean_8 = barcode.get_barcode_class('ean8')
-    convert_ean = ean_8(valor) # 74200153
-    qr_ean8 = convert_ean.save(valor)
-  if len(valor) == 12:
+    convert_ean = ean_8(value) # 74200153
+    qr_ean8 = convert_ean.save(value)
+  if len(value) == 12:
     upc_a = barcode.get_barcode_class('upca')
-    convert_upc = upc_a(valor) #877012000010
-    qr_ean_upc = convert_upc.save(valor)
+    convert_upc = upc_a(value) #877012000010
+    qr_ean_upc = convert_upc.save(value)
 
-def read_data():
-  with open('Salidapazosnuevo.txt', 'r') as f:
-    for linea in f:
-      if not linea.strip():
-        continue
-      if linea.split("|")[0] == "C" and linea.split("|")[1] == "1":
-        print(linea)
+
+with open('Salidapazosnuevo.txt', 'r') as f:
+  tickets = {}
+  for line in f:
+    if not line.strip():
+      continue
+    values_line = line.split("|")
+    if values_line[0] == "C" and values_line[1] == "1":
+      ticket_number = values_line[3]
+      tickets[ticket_number] = {'cashier_code': values_line[2], 'number_of_items': values_line[7], 'total_to_pay': values_line[8], 'qr_code': []}
+    elif values_line[0] == "L" and values_line[2] == "1":
+      tickets[ticket_number]['qr_code'].append(values_line[25])
+      
+for ticket_number, ticket_data in tickets.items():
+  for value in ticket_data["qr_code"]:
+    print(value)
+  # print(f'Ticket {ticket_number}: QRs = {ticket_data["qr_code"]}' )
 
 # generate_ean("877012000010")
-read_data()
+# read_data()
+print()
